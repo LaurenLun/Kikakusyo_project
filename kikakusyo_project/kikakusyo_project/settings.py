a96432b43d6190f71c030482edd15105e693ac86
Lauren <icelun0802@yahoo.co.jp>
2024-07-17 19:50:16 +0900

    main

--- kikakusyo_project/kikakusyo_project/settings.py
+++ kikakusyo_project/kikakusyo_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
+# from django.core.management.utils import get_andom_secret_key
 
 MESSAGE_TAGS = {
     messages.DEBUG: 'debug',
@@ -142,7 +143,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/LaurenLun/laurenlun.pythonanywhere.com/static'
 
 LOGIN_REDIRECT_URL = '/accounts/home'
 LOGOUT_REDIRECT_URL = '/accounts/us_login'
